bc47282018461e73063af50ff843e3721d702f9c
davidnite <davidnite@me.com>
2019-02-22 09:59:58 -0600

    add kerberos support and new tower-cli commands

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 1. Log in to Azure cloud shell and clone the repo
 2. Create a service principal and store the required values in the vars.yml file
 3. Update the vars.yml file with valid data for your environment
+4. Add Kerberos compatibility by uncommenting the related line in deploy.sh
 4. Run deploy.sh
 5. AWX will be publicly available, so update the NSG to restrict traffic
--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,12 @@ ansible-playbook awx_azure_infrastructure.yml --extra-vars "@vars.yml"
 # AWX Prerequisite Installation Playbook
 ansible-playbook awx_preinstallation.yml --extra-vars "@vars.yml"
 
+# Kerberos Configuration Playbook
+# ansible-playbook awx_kerberos_config.yml --extra-vars "@vars.yml"
+
 # AWX Installation Playbook
 ansible-playbook awx_install.yml --extra-vars "@vars.yml"
 
 # AWX Configuration Playbook (Tower-CLI)
+# Comment this line if you want to perform a manual configuration of AWX
 ansible-playbook awx_config.yml --extra-vars "@vars.yml"
